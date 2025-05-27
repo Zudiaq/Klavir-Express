fc96968e06ad20573be2b8e69c755355d2dc6d1f
Zodiac <aminmohammadi775@gmail.com>
2025-05-27 22:39:33 +0330

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,8 @@
 import requests
 import os
 import logging
-from mutagen.mp3 import MP3, id3, APIC, TIT2, TPE1, TALB  # Corrected import for id3
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC, TIT2, TPE1, TALB  # اصلاح ایمپورت
 from config import DEBUG_MODE, ENABLE_TELEGRAM
 
 logging.basicConfig(
@@ -31,7 +32,7 @@ def send_audio_with_caption(audio_path, caption, metadata=None):
     # Embed metadata into MP3 file
     if metadata:
         try:
-            audio = MP3(audio_path, ID3=id3.ID3)  # Corrected ID3 initialization
+            audio = MP3(audio_path, ID3=ID3)
             try:
                 audio.add_tags()
             except Exception:
