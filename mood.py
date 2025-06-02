08e9daf72c81ed668df4087e2da686ad790a76dc
Zodiac <aminmohammadi775@gmail.com>
2025-06-02 18:39:44 +0330

    Update mood.py

--- mood.py
+++ mood.py
@@ -42,56 +42,62 @@ def map_weather_to_mood(weather_data):
 
     time_slot = determine_detailed_time_of_day(hour)
     current_season = determine_season(month)
-    is_weekend = day_of_week >= 4  
+    is_weekend = day_of_week >= 5  
 
-    mood = "neutral_ambient"  
+    mood = "neutral_calm"  # Default mood
 
-    
+    if 9 <= hour < 10:
+        mood = "09:00_start_day_warm"
+    elif 10 <= hour < 12:
+        mood = "10:30_focus_motivation"
+    elif 12 <= hour < 13:
+        mood = "12:00_peak_energy"
+    elif 13 <= hour < 15:
+        mood = "13:30_post_lunch_rest"
+    elif 15 <= hour < 16:
+        mood = "15:00_mid_energy_variety"
+    elif 16 <= hour < 18:
+        mood = "16:30_afternoon_excitement"
+    elif 18 <= hour < 19:
+        mood = "18:00_work_release"
+    elif 19 <= hour < 21:
+        mood = "19:30_emotional_evening"
+    elif 21 <= hour < 22:
+        mood = "21:00_calm_night"
+    elif 22 <= hour < 24:
+        mood = "22:30_end_day_emotional"
 
+    # Adjust mood based on weather conditions
     if 'clear' in main_condition:
         if current_season == 'spring':
             mood = 'spring_uplifting_energetic' if time_slot in ['mid_morning', 'late_morning', 'afternoon'] else 'spring_peaceful_evening'
-            if is_weekend: mood = 'spring_weekend_joyful'
         elif current_season == 'summer':
             mood = 'summer_bright_energetic' if time_slot not in ['night', 'late_evening'] else 'summer_warm_night_chill'
-            if temp > 30 : mood = 'summer_hot_lazy' if time_slot in ['midday', 'afternoon'] else mood
-            if is_weekend and time_slot not in ['night', 'late_evening']: mood = 'summer_weekend_fun'
         elif current_season == 'autumn':
             mood = 'autumn_crisp_reflective' if time_slot in ['mid_morning', 'afternoon'] else 'autumn_cozy_evening'
-            if time_slot == 'early_evening': mood = 'autumn_golden_hour_folk'
         elif current_season == 'winter':
             mood = 'winter_bright_calm' if time_slot in ['mid_morning', 'late_morning'] else 'winter_still_night'
-            if temp < 5: mood = 'winter_cold_introspective'
 
     elif 'clouds' in main_condition:
         mood = 'cloudy_thoughtful_daydream'
         if 'overcast' in description: mood = 'overcast_melancholic_pensive'
-        if current_season in ['autumn', 'winter']: mood = 'cloudy_autumn_brooding'
-        if time_slot in ['late_evening', 'night']: mood = 'cloudy_night_mellow'
 
     elif 'rain' in main_condition or 'drizzle' in main_condition:
         mood = 'rainy_nostalgic_reading'
         if 'light rain' in description: mood = 'light_rain_cozy_acoustic'
         if 'thunderstorm' in description: mood = 'stormy_dramatic_epic'
-        if time_slot in ['late_evening', 'night']: mood = 'rainy_night_blues_jazz'
-        if is_weekend: mood = 'rainy_weekend_chill_lofi'
 
     elif 'snow' in main_condition:
         mood = 'snowy_wonderland_magical'
-        if is_weekend: mood = 'snowy_weekend_cozy_fireplace'
-        if time_slot in ['night']: mood = 'snowy_silent_night_ambient'
 
     elif 'mist' in main_condition or 'fog' in main_condition:
         mood = 'foggy_mysterious_atmospheric'
-        if time_slot in ['dawn', 'night']: mood = 'misty_ethereal_dreamlike'
 
-
+    # Special adjustments for specific days
     if day_of_week == 0 and time_slot in ['dawn', 'mid_morning']: 
         mood = 'monday_morning_focus_upbeat'
     elif day_of_week == 4 and time_slot in ['early_evening', 'late_evening']: 
         mood = 'friday_evening_party_celebration'
-    elif day_of_week == 5 and time_slot in ['mid_morning', 'afternoon']: 
-        mood = 'saturday_adventure_exploration'
 
     logging.info(f"Weather: {main_condition} ({description}), Temp: {temp}°C, Time: {time_slot} ({hour}h), Season: {current_season}, Day: {day_of_week}. Determined mood: {mood}")
     return mood
